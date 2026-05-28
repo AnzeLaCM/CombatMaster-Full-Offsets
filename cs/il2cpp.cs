cb04780566621ce1c2bc119ed0ef17b7fb7d53e9
AnzeLaCM <288473283+AnzeLaCM@users.noreply.github.com>
2026-05-28 02:19:00 +0100

    Auto-update offsets 2026-05-28 02:18:33 [Build 21934483]

--- cs/il2cpp.cs
+++ cs/il2cpp.cs
@@ -1,6 +1,6 @@
 /* OAAU - IL2CPP SDK
- * Generated: 2026-05-28 02:15:11
- * Build ID: 0x698F6488 */
+ * Generated: 2026-05-28 02:18:33
+ * Build ID: 21934483 */
 
 namespace OAAU.SDK
 {
@@ -16,4 +16,4 @@ namespace OAAU.SDK
     public struct Quaternion { public float x, y, z, w; }
     public struct Color { public float r, g, b, a; }
 }
-wgZywgYiwgYTsgfTsK
+IGcsIGIsIGE7IH07Cg
