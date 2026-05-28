1df8c01d974d2fd92df5e084b4f96b185dd1c862
AnzeLaCM <288473283+AnzeLaCM@users.noreply.github.com>
2026-05-28 01:53:45 +0100

    Auto-update offsets 2026-05-28 01:53:19 [0x698F6488]

---
+++ cs/il2cpp.cs
@@ -0,0 +1,20 @@
+/* OAAU - IL2CPP SDK
+ * Generated: 2026-05-28 01:53:19
+ * Build ID: 0x698F6488 */
+
+namespace OAAU.SDK
+{
+    public class Il2CppObject { public System.IntPtr klass; public System.IntPtr monitor; }
+    public class Il2CppString : Il2CppObject { public int length; public char[] chars; }
+    public class Il2CppArray  : Il2CppObject { public System.IntPtr bounds; public ulong max_length; }
+    public class Il2CppList   : Il2CppObject { public Il2CppArray _items; public int _size; }
+
+    public struct Vector2  { public float x, y; }
+    public struct Vector3  { public float x, y, z; }
+    public struct Vector4  { public float x, y, z, w; }
+    public struct Matrix4x4 { public float[,] m; }
+    public struct Quaternion { public float x, y, z, w; }
+    public struct Color { public float r, g, b, a; }
+}
+{ *(T*)a = v; }
+}
